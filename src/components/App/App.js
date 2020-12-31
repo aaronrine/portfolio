d2083e8cb12dcfb0e957a0a7e8b04428ef609174
Aaron Rine <aaronrine@gmail.com>
2020-12-31 13:25:12 -0800

    feat(App): skeleton html

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,22 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <header>
+        <h1>Portfolio Site</h1>
+        <h4>This is my portfolio site</h4>
+      </header>
+      <nav>
+        <a>home</a>
+        <a>project 1</a>
+        <a>project 2</a>
+        <a>project 3</a>
+      </nav>
+      <main>
+        <p>This is project 1</p>
+        <p>This is project 2</p>
+        <p>This is project 3</p>
+      </main>
+      <footer>Footer</footer>
     </div>
   );
 }
