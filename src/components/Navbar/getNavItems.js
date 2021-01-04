85b12af36a4b02e783a5334bd66408599d4d20fe
Aaron Rine <aaronrine@gmail.com>
2021-01-04 15:52:14 -0800

    chore(getNavItems):  change navItem data

--- src/components/Navbar/getNavItems.js
+++ src/components/Navbar/getNavItems.js
@@ -10,84 +10,12 @@ function assignUuid(item) {
 export function getNavItems() {
   const navItems = [
     {
-      name: "Home",
-      submenu: [
-        {
-          name: "About Me",
-          href: "#",
-          submenu: [
-            {
-              name: "Home1",
-              submenu: [
-                {
-                  name: "About Me2",
-                  href: "#",
-                },
-                {
-                  name: "Projects3",
-                  href: "#",
-                },
-              ],
-            },
-            {
-              name: "To-Do App4",
-              submenu: [
-                {
-                  name: "About Me5",
-                  href: "#",
-                },
-                {
-                  name: "Projects6",
-                  href: "#",
-                },
-              ],
-            },
-            {
-              name: "E-commerce Site7",
-              submenu: [
-                {
-                  name: "About Me8",
-                  href: "#",
-                },
-                {
-                  name: "Projects9",
-                  href: "#",
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: "Projects10",
-          href: "#",
-        },
-      ],
+      name: "Who I am",
+      href: "aboutme",
     },
     {
-      name: "To-Do App11",
-      submenu: [
-        {
-          name: "About Me12",
-          href: "#",
-        },
-        {
-          name: "Projects13",
-          href: "#",
-        },
-      ],
-    },
-    {
-      name: "E-commerce Site14",
-      submenu: [
-        {
-          name: "About Me15",
-          href: "#",
-        },
-        {
-          name: "Projects16",
-          href: "#",
-        },
-      ],
+      name: "My Work",
+      href: "projects",
     },
   ];
   navItems.forEach((item) => assignUuid(item));
