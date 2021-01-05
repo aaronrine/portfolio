1b52ad78e095732cc1d6d81f4d6f3919858c2f05
Aaron Rine <aaronrine@gmail.com>
2021-01-05 13:18:55 -0800

    style(Home): coarse sanding of styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,13 +7,35 @@ export function Home() {
   return (
     <main className="Home">
       <section className="bio">
-        <div>
-          <img src={profile} alt="A photo of Aaron Rine" />
-          <p>short quote</p>
+        <div className="bioCard">
+          <div className="imageWrapper">
+            <img src={profile} alt="A photo of Aaron Rine" />
+          </div>
+          <p>
+            Websites hold a tremoundous amount of power and it is my ambition to
+            join the larger community of web developers in helping to give this
+            power to companies and individuals. Much of my ability to provide
+            comes from being a quick learner, as most coding obstacles are
+            challenges we haven't yet seen. Coupled with experience working on
+            e-commerce sites and over a year of intensive learning, I am
+            confident that I can find the solutions your company needs to go to
+            the next level.
+          </p>
         </div>
-        <p>intro w/ skills</p>
-        <p>mid w/ passion for web dev</p>
-        <p>conclusion w/ ways i add value</p>
+        <div className="bioText">
+          <p>
+            Most of my experience lies in front-end development. Having an
+            advanced grasp of HTML5, JavaScript, CSS and others allowed me to
+            seamlessly move between vanilla JS toy problems and business level
+            framework issues such as e-commerce with react or using third party
+            API's.
+          </p>
+          <p>
+            Rome wasn't built in a day and it certainly wasn't built by one
+            person. While most of my work is my own, I am no stranger to working
+            with friends and colleagues on coding projects and sharing repos.
+          </p>
+        </div>
       </section>
       <ProjectCarousel projects={data.projects} />
       <section className="contact">
