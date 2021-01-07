4e88dd3252b95f8ccb95a7457a750495fecacf20
Aaron Rine <aaronrine@gmail.com>
2021-01-07 13:22:19 -0800

    style(Header): add aside styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
 export function Header() {
   return (
-    <header>
-      <Link to="/" className="Link">
-        <h1>Portfolio Site</h1>
-      </Link>
-      <h4>This is my portfolio site</h4>
+    <header className="Header">
+      <div className="title">
+        <Link to="/" className="Link">
+          <h1>Portfolio Site</h1>
+        </Link>
+        <h4>This is my portfolio site</h4>
+      </div>
+      <aside className="details">
+        <p>Aaron rine</p>
+        <a href="https://www.linkedin.com/in/aaron-rine-1903b511a/">
+          Follow me on LinkedIn to see my latest progress
+        </a>
+        <a href="https://github.com/aaronrine">Check out my Github</a>
+      </aside>
     </header>
   );
 }
