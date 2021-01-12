b88e015e817d329781171ffb523adb829acc96a0
Aaron Rine <aaronrine@gmail.com>
2021-01-12 14:40:27 -0800

    feat(Navbar): add collapsable menu

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,20 @@
 import "./Navbar.scss";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 export function Navbar({ navItems }) {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="Navbar">
-      {navItems?.map((navItem) => {
-        return <NavItem {...navItem} key={navItem.uuid} />;
-      })}
+      <button onClick={() => setShowMenu(!showMenu)}>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+
+      {showMenu &&
+        navItems?.map((navItem) => {
+          return <NavItem {...navItem} key={navItem.uuid} />;
+        })}
     </nav>
   );
 }
